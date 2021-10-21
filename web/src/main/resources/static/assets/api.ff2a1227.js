7fd988822f1bf8c8566dbf8885e4869532550554
wangjun <wjun_java@163.com>
2021-10-21 09:29:38 +0800

    ui

---
+++ web/src/main/resources/static/assets/api.ff2a1227.js
@@ -0,0 +1,1 @@
+import{C as t,e as r,S as s,f as o}from"./index.ca65de1b.js";function i(n){var e=t.extend(n);return t.registerClass(e),e}function d(n){var e=r.extend(n);return r.registerClass(e),e}function C(n){var e=s.extend(n);return s.registerClass(e),e}function l(n){var e=o.extend(n);return o.registerClass(e),e}export{d as a,C as b,l as c,i as e};
--- web/src/main/resources/static/libs/sku/getSetSkuVals.js
+++
@@ -1,9 +0,0 @@
-//获取已经设置的SKU
-$(".getSetSkuVal").on("click",function(){
-	$("tr[class*='sku_table_tr']").each(function(){
-		var propids = $(this).attr("propids");//SKU类型主键
-		var propvalids = $(this).attr("propvalids");//SKU值主键
-		
-		
-	});
-});
