ef95a006906a44f4339fc3ce9aa0cf55a1066e54
wangjun <wjun_java@163.com>
2021-10-27 19:56:18 +0800

    auth

---
+++ web/src/main/java/com/fastcms/web/security/AuthManager.java
@@ -0,0 +1,14 @@
+package com.fastcms.web.security;
+
+/**
+ *  @author： wjun_java@163.com
+ *  * @date： 2021/10/24
+ *  * @description：
+ *  * @modifiedBy：
+ *  * @version: 1.0
+ */
+public interface AuthManager {
+
+
+
+}
