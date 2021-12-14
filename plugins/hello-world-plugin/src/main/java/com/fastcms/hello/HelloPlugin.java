f70bbc9b61b3940edff6ecb96db65c5372bc1aa9
wangjun <wjun_java@163.com>
2021-12-14 20:49:53 +0800

    plugin

---
+++ plugins/hello-world-plugin/src/main/java/com/fastcms/hello/HelloPlugin.java
@@ -0,0 +1,16 @@
+package com.fastcms.hello;
+
+import com.fastcms.plugin.PluginBase;
+import org.pf4j.PluginWrapper;
+
+/**
+ * hello world plugin
+ * wjun_java@163.com
+ */
+public class HelloPlugin extends PluginBase {
+
+    public HelloPlugin(PluginWrapper wrapper) {
+        super(wrapper);
+    }
+
+}
