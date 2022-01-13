d8bcf6a5a82c7b3695b6b51223b82d4fae5160f3
wangjun <wjun_java@163.com>
2022-01-13 17:44:01 +0800

    plugin

--- web/src/main/resources/static/assets/index.752833d5.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./index.5eb239a7.js";import{m as r,D as n}from"./vendor.a9aef9fe.js";const o={name:"centerHome"};function a(t,s,c,f,m,p){return n(),r("div",null,"\u6211\u7684\u6587\u7AE0")}var i=e(o,[["render",a]]);export{i as default};
--- web/src/main/resources/static/assets/index.b5f4c2fe.js
+++
@@ -1,1 +0,0 @@
-import{_ as o}from"./index.5eb239a7.js";import{l as t,m as n,z as r,D as a}from"./vendor.a9aef9fe.js";const c={name:"centerHome"},s={style:{padding:"30px","text-align":"center"}};function p(_,i,l,d,m,f){const e=t("el-color-picker");return a(),n("div",s,[r(e,{"show-alpha":""})])}var k=o(c,[["render",p]]);export{k as default};
