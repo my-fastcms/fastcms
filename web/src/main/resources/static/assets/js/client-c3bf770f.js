192aa1406293d953116f56b9fc878904d9c5643d
wangjun <wjun_java@163.com>
2023-08-14 12:01:28 +0800

    ui

---
+++ web/src/main/resources/static/assets/js/client-c3bf770f.js
@@ -0,0 +1,1 @@
+import{s as e}from"./index-aa5b7dfa.js";function s(){return{updateUser(r){return e({url:"/client/user/save",method:"post",data:r})},getUserInfo(){return e({url:"/client/user/get",method:"get"})}}}export{s as C};
